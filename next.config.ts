52253659ed7dfef65f5522599e465dc97179b368
Tuhin Das <dastuhin.rnj@gmail.com>
2025-09-10 12:27:02 +0530

    Cloudflare Update

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,11 @@ const nextConfig: NextConfig = {
         // This is the specific hostname from your R2 bucket.
         hostname: 'pub-32361a86652b4a0f95ba4bed9f014ed0.r2.dev',
       },
+      {
+        protocol: 'https',
+        // This is the specific hostname from your R2 bucket.
+        hostname: 'fitness-plus-revamped.vercel.app',
+      },
     ],
   },
 };
